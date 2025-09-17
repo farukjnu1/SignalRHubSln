264b5256ed80119c70aa4fa34d7db10b7f387d9b
farukjnu1@gmail.com <farukjnu1@gmail.com>
2025-09-17 20:03:18 +0800

    Add project files.

---
+++ SignalRHub/Controllers/valuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalRHub.Models;
+
+namespace SignalRHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class valuesController : ControllerBase
+    {
+        private BusTicketContext _ctx = null;
+
+        public valuesController()
+        {
+            _ctx = new BusTicketContext();
+        }
+
+        [HttpGet("[action]")]
+        public object index()
+        {
+            object result = null; string message = string.Empty;
+            message = "API running...";
+
+            result = new
+            {
+                message
+            };
+            return result;
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<Divisions> GetDivisions()
+        {
+            return _ctx.Divisions.OrderBy(o => o.Name);
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<Districts> GetDistrictsByDivision(int divisionId)
+        {
+            return _ctx.Districts.Where(w => w.DivisionId == divisionId).OrderBy(o => o.Name);
+        }
+    }
+}
---
+++ SignalRHub/Services/StaticInfos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRHub.Services
+{
+    public static class StaticInfos
+    {
+        public const string connecitonString = @"Server=FARUK-PC;Database=BusTicket;User Id=sa; Password=123;";
+
+        public enum ScheduleStatus { reserved, empty}
+
+        public static string[] Role = { "Sale Executive", "Manager"};
+    }
+}
